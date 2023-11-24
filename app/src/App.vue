a22deb6f6cb687c9aa00ad85cf0d3a6bfb4ecff1
EdiJunior88 <er.moura88@yahoo.com.br>
2023-11-24 20:53:28 -0300

    - Introdução
    - Introdução ao Vue.js
    Adicionar o Vue.js a uma página
    Criar um aplicativo
    Adicionar dados
    Criar um objeto de dados
    Montar o aplicativo
    Exibir dados

---
+++ app/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+let firstName = 'Edivaldo'
+let lastName = 'Junior'
+</script>
+
+<template>
+  <div>{{ firstName }}, {{ lastName }}</div>
+</template>
+
+<style lang="css" scoped></style>
