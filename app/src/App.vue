914901d32a6205889f29c4148358c0c9e2da2a4b
EdiJunior88 <er.moura88@yahoo.com.br>
2023-12-08 18:01:55 -0300

    - Exibições de página dinâmicas com o Vue.js
    - Introdução às exibições dinâmicas
    Objetivos
    Pré-requisitos
    
    - Renderizar listas
    Introdução à diretiva v-for
    Usar v-for em um aplicativo

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,21 +1,11 @@
 <script setup lang="js">
-import productImage from '@/assets/cruise.jpg'
-
-let productImageDescription =
-  'Um astronauta flutua do lado de fora da janela enquanto você se senta confortavelmente'
-let productImageStyle = { 'border-radius': '15px' }
-
-let productName = 'Reserve um cruzeiro para a Lua'
-let productDescription =
-  'Faça um cruzeiro até a lua em nosso luxuoso ônibus espacial. Veja os astronautas trabalhando do lado de fora da Estação Espacial Internacional'
+const names = ['Susan', 'Peter', 'Bill']
 </script>
 
 <template>
-  <div class="nav-bar"></div>
-  <h1>Excursões pela Galáxia da Relecloud</h1>
-  <h2>{{ productName }}</h2>
-  <div>{{ productDescription }}</div>
-  <img :src="productImage" :alt="productImageDescription" :style="productImageStyle" />
+  <ul>
+    <li v-for="name in names">📌 {{ name }}</li>
+  </ul>
 </template>
 
 <style lang="css" scoped></style>
