37c52da5f936ba021f2634d8403e4b3c22549cd8
EdiJunior88 <er.moura88@yahoo.com.br>
2023-12-11 16:54:48 -0300

    - Exibições de página dinâmicas com o Vue.js
    - Introdução às exibições dinâmicas
    Objetivos
    Pré-requisitos
    
    - Renderizar listas
    Introdução à diretiva v-for
    Usar v-for em um aplicativo
    Chaves e estado
    
    - Praticar a renderização de listas
    Clonar o repositório inicial e explorar o código
    Exibir a lista de opções e preços
    Explore o código
    Abrir a página no Live Server
    
    - Usar a renderização condicional
    Alternar a visibilidade
    Lógica booliana

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,7 +1,10 @@
 <script setup lang="js"></script>
 
 <template>
-  <div v-show="new Date().getMonth() < 3">É o primeiro trimestre</div>
+  <div v-if="new Date().getMonth() < 3">É o primeiro trimestre</div>
+  <div v-if="new Date().getMonth() < 6">É o segundo trimestre</div>
+  <div v-if="new Date().getMonth() < 9">É o terceiro trimestre</div>
+  <div>É o quarto trimestre</div>
 </template>
 
 <style lang="css" scoped></style>
