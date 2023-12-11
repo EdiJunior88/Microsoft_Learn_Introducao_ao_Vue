6a0e3c7e2127ae4790f4b03b839efbc6aabae068
EdiJunior88 <er.moura88@yahoo.com.br>
2023-12-11 16:53:00 -0300

    - Exibições de página dinâmicas com o Vue.js
    - Introdução às exibições dinâmicas
    Objetivos
    Pré-requisitos
    
    - Renderizar listas
    Introdução à diretiva v-for
    Usar v-for em um aplicativo
    Chaves e estado
    
    - Praticar a renderização de listas
    Clonar o repositório inicial e explorar o código
    Exibir a lista de opções e preços
    Explore o código
    Abrir a página no Live Server
    
    - Usar a renderização condicional
    Alternar a visibilidade

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,60 +1,7 @@
-<script setup lang="js">
-import productImage from '@/assets/cruise.jpg'
-
-let productImageDescription =
-  'Um astronauta flutua do lado de fora da janela enquanto você se senta confortavelmente'
-let productImageStyle = { 'border-radius': '15px' }
-
-let productName = 'Reserve um cruzeiro para a Lua'
-let productDescription =
-  'Faça um cruzeiro até a lua em nosso luxuoso ônibus espacial. Veja os astronautas trabalhando do lado de fora da Estação Espacial Internacional'
-
-let productClasses = [
-  {
-    nome: 'Econômica',
-    preco: 125000,
-    assentosDisponiveis: 20,
-    compraAntecipada: true
-  },
-  {
-    nome: 'Executiva',
-    preco: 275000,
-    assentosDisponiveis: 6,
-    compraAntecipada: true
-  },
-  {
-    nome: 'VIP',
-    preco: 430000,
-    assentosDisponiveis: 2,
-    compraAntecipada: false
-  }
-]
-</script>
+<script setup lang="js"></script>
 
 <template>
-  <div class="nav-bar"></div>
-
-  <div id="app">
-    <h1>Excursões pela Galáxia da Relecloud</h1>
-    <h2>{{ productName }}</h2>
-    <div>{{ productDescription }}</div>
-    <hr />
-
-    <div v-for="(productClass, index) in productClasses" :key="index" class="row">
-      <div class="column">{{ productClass.nome }}</div>
-      <div class="column">
-        {{
-          productClass.preco.toLocaleString('pt-BR', {
-            style: 'currency',
-            currency: 'BRL'
-          })
-        }}
-      </div>
-    </div>
-
-    <!-- TODO: Adicionar código para exibir classes -->
-    <img :src="productImage" :alt="productImageDescription" :style="productImageStyle" />
-  </div>
+  <div v-show="new Date().getMonth() < 3">É o primeiro trimestre</div>
 </template>
 
 <style lang="css" scoped></style>
