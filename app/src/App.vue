c36aea859ea7f6827dc3d9785b8f8cb2f87a0241
EdiJunior88 <er.moura88@yahoo.com.br>
2023-12-11 18:12:08 -0300

    - Trabalhar com eventos e dados no Vue.js
    - Visão geral de dados dinâmicos e eventos
    Pré-requisitos
    
    - Trabalhar com formulários
    v-model
    Associação a caixas de texto

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,64 +1,13 @@
 <script setup lang="js">
-import productImage from '@/assets/cruise.jpg'
-
-const productImageDescription =
-  'Um astronauta flutua do lado de fora da janela enquanto você se senta confortavelmente'
-const productImageStyle = { 'border-radius': '15px' }
-
-const productName = 'Reserve um cruzeiro para a Lua'
-const productDescription =
-  'Faça um cruzeiro até a lua em nosso luxuoso ônibus espacial. Veja os astronautas trabalhando do lado de fora da Estação Espacial Internacional'
-
-const produtoClasses = [
-  {
-    nome: 'Econômica',
-    preco: 125000,
-    assentosDisponiveis: 20,
-    compraAntecipada: true
-  },
-  {
-    nome: 'Executiva',
-    preco: 275000,
-    assentosDisponiveis: 6,
-    compraAntecipada: true
-  },
-  {
-    nome: 'VIP',
-    preco: 430000,
-    assentosDisponiveis: 2,
-    compraAntecipada: false
-  }
-]
+const name = 'Cheryl'
+const status = -1
+const active = true
+const benefíciosSelecionados = 'yes'
+const statusList = ['tempo integral', 'meio período', 'contratante']
 </script>
 
 <template>
-  <div class="nav-bar"></div>
-
-  <div id="app">
-    <h1>Excursões pela Galáxia da Relecloud</h1>
-    <h2>{{ productName }}</h2>
-    <div>{{ productDescription }}</div>
-    <hr />
-
-    <div v-for="(produtoClasse, index) in produtoClasses" :key="index" class="row">
-      <div class="column">{{ produtoClasse.nome }}</div>
-      <div class="column">
-        {{
-          produtoClasse.preco.toLocaleString('pt-BR', {
-            style: 'currency',
-            currency: 'BRL'
-          })
-        }}
-      </div>
-
-      <!-- Mais conteúdos -->
-      <button v-if="produtoClasse.compraAntecipada" class="button">Reserva antecipada</button>
-      <button v-else class="button">Reserve agora</button>
-
-      <div v-show="produtoClasse.assentosDisponiveis < 10">Quase esgotada!</div>
-    </div>
-
-    <!-- TODO: Adicionar código para exibir classes -->
-    <img :src="productImage" :alt="productImageDescription" :style="productImageStyle" />
-  </div>
+  <input type="text" v-model="name" />
 </template>
+
+<style lang="css" scoped></style>
