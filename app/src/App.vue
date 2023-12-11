7124caca8179241936a031a1a31b779615cfd65a
EdiJunior88 <er.moura88@yahoo.com.br>
2023-12-11 17:12:38 -0300

    - Exibições de página dinâmicas com o Vue.js
    - Introdução às exibições dinâmicas
    Objetivos
    Pré-requisitos
    
    - Renderizar listas
    Introdução à diretiva v-for
    Usar v-for em um aplicativo
    Chaves e estado
    
    - Praticar a renderização de listas
    Clonar o repositório inicial e explorar o código
    Exibir a lista de opções e preços
    Explore o código
    Abrir a página no Live Server
    
    - Usar a renderização condicional
    Alternar a visibilidade
    Lógica booliana
    
    - Praticar o uso de condições
    Adicionar código para atualizar a exibição
    Testar a exibição

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,10 +1,66 @@
-<script setup lang="js"></script>
+<script setup lang="js">
+import productImage from '@/assets/cruise.jpg'
+
+let productImageDescription =
+  'Um astronauta flutua do lado de fora da janela enquanto você se senta confortavelmente'
+let productImageStyle = { 'border-radius': '15px' }
+
+let productName = 'Reserve um cruzeiro para a Lua'
+let productDescription =
+  'Faça um cruzeiro até a lua em nosso luxuoso ônibus espacial. Veja os astronautas trabalhando do lado de fora da Estação Espacial Internacional'
+
+let produtoClasses = [
+  {
+    nome: 'Econômica',
+    preco: 125000,
+    assentosDisponiveis: 20,
+    compraAntecipada: true
+  },
+  {
+    nome: 'Executiva',
+    preco: 275000,
+    assentosDisponiveis: 6,
+    compraAntecipada: true
+  },
+  {
+    nome: 'VIP',
+    preco: 430000,
+    assentosDisponiveis: 2,
+    compraAntecipada: false
+  }
+]
+</script>
 
 <template>
-  <div v-if="new Date().getMonth() < 3">É o primeiro trimestre</div>
-  <div v-if="new Date().getMonth() < 6">É o segundo trimestre</div>
-  <div v-if="new Date().getMonth() < 9">É o terceiro trimestre</div>
-  <div>É o quarto trimestre</div>
+  <div class="nav-bar"></div>
+
+  <div id="app">
+    <h1>Excursões pela Galáxia da Relecloud</h1>
+    <h2>{{ productName }}</h2>
+    <div>{{ productDescription }}</div>
+    <hr />
+
+    <div v-for="(produtoClasse, index) in produtoClasses" :key="index" class="row">
+      <div class="column">{{ produtoClasse.nome }}</div>
+      <div class="column">
+        {{
+          produtoClasse.preco.toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          })
+        }}
+      </div>
+
+      <!-- Mais conteúdos -->
+      <button v-if="produtoClasse.compraAntecipada" class="button">Reserva antecipada</button>
+      <button v-else class="button">Reserve agora</button>
+
+      <div v-show="produtoClasse.assentosDisponiveis < 10">Quase esgotada!</div>
+    </div>
+
+    <!-- TODO: Adicionar código para exibir classes -->
+    <img :src="productImage" :alt="productImageDescription" :style="productImageStyle" />
+  </div>
 </template>
 
 <style lang="css" scoped></style>
