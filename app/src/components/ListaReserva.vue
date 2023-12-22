744ab46b22b1f8c4ca82bd1d59445a21b37ec8ce
EdiJunior88 <er.moura88@yahoo.com.br>
2023-12-21 23:02:04 -0300

    - Introdução à CLI do Vue e a componentes de arquivo único no Vue.js.
    - Introdução à CLI e aos componentes personalizados do Vue
    
    - Introdução à CLI do Vue
    Inicialização
    Processo de compilação
    Servidor de desenvolvimento
    
    - Criar um aplicativo usando a CLI do Vue
    Instalar a CLI do Vue
    Inicializar um aplicativo
    Explore o código
    Executar o servidor de desenvolvimento
    
    - Introdução aos componentes do Vue
    Componentes do Vue
    Estrutura de componentes do Vue (estilo ///
    Script /// template)
    Carregamento e componentes
    Separação de interesses
    
    - Criar um componente
    Instalar extensões do Visual Studio Code
    Criar o componente de Host
    Atualizar a seção de script
    Adicionar o modelo
    Adicionar o estilo
    Definir o componente de Host como o ponto de entrada para o aplicativo
    Testar o aplicativo
    
    - Props de componentes
    Definindo props
    Restringindo tipos
    Objetos complexos
    Usando props dentro de um componente
    
    - Adicionar props a um componente
    Criar o componente
    Registrar os props e o valor computado

---
+++ app/src/components/ListaReserva.vue
@@ -0,0 +1,29 @@
+<script setup lang="js">
+import { computed } from 'vue'
+
+const reservas = defineProps({
+  reservas: Array
+})
+
+const totalPainel = computed(() => {
+  let custoTotal = 0
+
+  if (reservas && reservas.length > 0) {
+    custoTotal = reservas.map((b) => b.preco).reduce((a, b) => a + b)
+  }
+
+  return `${custoTotal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}`
+})
+</script>
+
+<template>
+  <section>
+    <h2>Reservas atuais:</h2>
+
+    <div class="row" v-for="(reserva, index) in reservas" :key="index">
+      <div>{{ reserva.nome }}</div>
+    </div>
+
+    <h3 class="row">Total: {{ totalPainel }}</h3>
+  </section>
+</template>
