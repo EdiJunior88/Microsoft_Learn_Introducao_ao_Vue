2a80afedf32dfe3f8403467c0ec92267c8599432
EdiJunior88 <er.moura88@yahoo.com.br>
2023-12-21 21:20:20 -0300

    - Introdução à CLI do Vue e a componentes de arquivo único no Vue.js.
    - Introdução à CLI e aos componentes personalizados do Vue
    
    - Introdução à CLI do Vue
    Inicialização
    Processo de compilação
    Servidor de desenvolvimento
    
    - Criar um aplicativo usando a CLI do Vue
    Instalar a CLI do Vue
    Inicializar um aplicativo
    Explore o código
    Executar o servidor de desenvolvimento
    
    - Introdução aos componentes do Vue
    Componentes do Vue
    Estrutura de componentes do Vue (estilo ///
    Script /// template)
    Carregamento e componentes
    Separação de interesses

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,30 +1,7 @@
-<script setup lang="js">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script src="./hello.js"></script>
 
 <template>
-  <div class="logo">
-    <img alt="Vue logo" src="./assets/logo.png" />
-  </div>
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>Hello, world</div>
 </template>
 
-<style lang="css" scoped>
-#app {
-  font-family: Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-img {
-  width: 200px;
-}
-
-.logo {
-  display: flex;
-  justify-content: center;
-}
-</style>
+<style src="./style.css"></style>
